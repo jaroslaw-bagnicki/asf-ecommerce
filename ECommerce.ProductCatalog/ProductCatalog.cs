dfa73bd053f5fe9db7c6a37bdd0dedfa0effa40b
Jarosław Bagnicki <jarek@bagnicki.net>
2022-04-30 10:33:30 +0200

    Update ProductCatalog service

--- ECommerce.ProductCatalog/ProductCatalog.cs
+++ ECommerce.ProductCatalog/ProductCatalog.cs
@@ -1,51 +1,74 @@
 using System;
 using System.Collections.Generic;
 using System.Fabric;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace ECommerce.ProductCatalog
 {
     /// <summary>
-    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class ProductCatalog : StatelessService
+    internal sealed class ProductCatalog : StatefulService
     {
-        public ProductCatalog(StatelessServiceContext context)
+        public ProductCatalog(StatefulServiceContext context)
             : base(context)
         { }
 
         /// <summary>
-        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
         /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
         /// <returns>A collection of listeners.</returns>
-        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceInstanceListener[0];
+            return new ServiceReplicaListener[0];
         }
 
         /// <summary>
-        /// This is the main entry point for your service instance.
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
         /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
+            ServiceFabricProductRepository _productRepository = new ServiceFabricProductRepository(StateManager);
 
-            long iterations = 0;
-
-            while (true)
+            var task1 = _productRepository.Add(new Product
             {
-                cancellationToken.ThrowIfCancellationRequested();
+                Id = Guid.NewGuid(),
+                Name = "Dell Monitor",
+                Description = "Computer Conitor",
+                Price = 500,
+                Availability = 100,
+            });
 
-                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+            var task2 = _productRepository.Add(new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Surface Book",
+                Description = "Microsoft's Latest Laptop, i7 CPU, 3GB RAM",
+                Price = 2200,
+                Availability = 15
+            });
 
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
-            }
+            var task3 = _productRepository.Add(new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Arc Touch Mouse",
+                Description = "Computer Mouse, bluetooth, reqiures 2 AAA batteries",
+                Price = 60,
+                Availability = 30,
+            });
+
+            await Task.WhenAll(task1, task2, task3);
+
+            var all = await _productRepository.GetAll();
         }
     }
 }
