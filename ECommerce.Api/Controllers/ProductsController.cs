c5bd61abac7574641f3ca293e3feb3d0cb150d19
Jarosław Bagnicki <jarek@bagnicki.net>
2022-05-01 09:06:21 +0200

    Add testing endpoint

---
+++ ECommerce.Api/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using ECommerce.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<ApiProduct>> Get()
+        {
+            var result = new[]
+            {
+                new ApiProduct
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "fake"
+                }
+            };
+
+            return Task.FromResult<IEnumerable<ApiProduct>>(result);
+        }
+
+        [HttpPost]
+        public Task Create()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ECommerce.Api/Models/ApiProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace ECommerce.Api.Models
+{
+    public class ApiProduct
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+
+        [JsonPropertyName("isAvailable")]
+        public bool IsAvailable { get; set; }
+    }
+}
