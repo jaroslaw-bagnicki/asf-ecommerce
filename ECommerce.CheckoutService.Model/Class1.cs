85db4aed038d71a8e4567e8050ce3314c5de571e
Jarosław Bagnicki <jarek@bagnicki.net>
2022-05-25 21:01:20 +0200

    Add CheckoutService models

--- ECommerce.CheckoutService.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ECommerce.CheckoutService.Model
-{
-    public class Class1
-    {
-    }
-}
--- ECommerce.CheckoutService/CheckoutService.cs
+++ ECommerce.CheckoutService/CheckoutService.cs
@@ -4,6 +4,7 @@ using System.Fabric;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ECommerce.CheckoutService.Model;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
@@ -13,11 +14,21 @@ namespace ECommerce.CheckoutService
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class CheckoutService : StatefulService
+    internal sealed class CheckoutService : StatefulService, ICheckoutService
     {
         public CheckoutService(StatefulServiceContext context)
             : base(context)
         { }
+
+        public Task<CheckoutSummary> CheckoutAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CheckoutSummary[]> GetOrderHistoryAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
